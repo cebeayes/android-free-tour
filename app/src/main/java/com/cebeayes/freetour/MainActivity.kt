7a4a021e5cc966645fdfac82322bce3897dab2dd
Cemal <cemal.aytekin@boun.edu.tr>
2021-10-11 20:36:34 +0200

    upload android project

---
+++ app/src/main/java/com/cebeayes/freetour/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cebeayes.freetour
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
